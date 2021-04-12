8f46cb8d4753d17fa18e0f1d72b4696f4ce4df81
celia-b <celiburgos@gmail.com>
2021-04-12 08:31:42 +0000

    new dir structure

--- diabetes_app/app.R
+++ diabetes_app/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Diabetes Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
